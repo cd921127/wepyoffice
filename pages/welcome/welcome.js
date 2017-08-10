48a8e8a91a62392c0d6766c15d5fa3c746c49bda
chengdu <chengdu@zallds.com>
2017-08-10 19:00:53 +0800

    wepy教程源码改造

---
+++ pages/welcome/welcome.js
@@ -0,0 +1,23 @@
+Page({
+    onTapJump: function (event) {
+      debugger;
+        wx.switchTab({
+            url: "../post/post",
+            success: function () {
+                console.log("jump success")
+            },
+            fail: function () {
+                console.log("jump failed")
+            },
+            complete: function () {
+                console.log("jump complete")
+            }
+        });
+    },
+    onUnload: function (event) {
+        console.log("page is unload")
+    },
+    onHide: function (event) {
+        console.log("page is hide")
+    },
+})
